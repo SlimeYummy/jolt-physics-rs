e56ccd3d96805ce1d1916d257cdd278fb0bfac19
Nan <email@fenqi.moe>
2024-07-05 23:00:04 +0800

    Support build on linux

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
 mod base;
 mod character;
-mod debug;
 mod layer;
 mod shape;
 mod system;
 
 pub use base::{BodyID, IndexedTriangle, RefPhysicsMaterial, RefPhysicsSystem, RefShape};
 pub use character::*;
-pub use debug::*;
 pub use layer::*;
 pub use shape::*;
 pub use system::*;
+
+#[cfg(all(windows, feature = "debug-renderer"))]
+mod debug;
+
+#[cfg(all(windows, feature = "debug-renderer"))]
+pub use debug::*;
